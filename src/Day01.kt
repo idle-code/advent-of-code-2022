9b3bc9e7ebf11915607391a8f40a3e075b826911
Pawel Zukowski <p.z.idlecode@gmail.com>
2022-12-02 13:40:17 +0100

    Day 1 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,42 @@
+data class CalorieEntry(val elfId: Int, val calories: Int)
+
+fun parseInput(inputLines: List<String>): Iterable<CalorieEntry> {
+    val caloriesPerItem = mutableListOf<CalorieEntry>()
+    var elfId = 0
+    for (line in inputLines) {
+        if (line.isEmpty()) {
+            ++elfId
+            continue
+        }
+        caloriesPerItem.add(CalorieEntry(elfId, line.toInt()))
+    }
+
+    val totalCaloriesPerElf = caloriesPerItem.groupBy { entry ->
+        entry.elfId
+    }.map {
+        CalorieEntry(it.key, it.value.sumOf { calorieEntry -> calorieEntry.calories })
+    }
+
+    return totalCaloriesPerElf
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val totalCaloriesPerElf = parseInput(input)
+
+        val maxCalorieElf = totalCaloriesPerElf.maxBy { it.calories }
+        return maxCalorieElf.calories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val totalCaloriesPerElf = parseInput(input)
+
+        return totalCaloriesPerElf.sortedByDescending { it.calories }.take(3).sumOf { it.calories }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
