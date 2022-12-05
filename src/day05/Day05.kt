85a2899e7995f899bece8776394dfe1e573a1ae2
Pawel Zukowski <p.z.idlecode@gmail.com>
2022-12-05 14:33:12 +0100

    Refactor of Day 5; Day 6 stub

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -6,13 +6,21 @@ typealias Crate = Char
 
 val commandLineRegex = """move (\d+) from (\d+) to (\d+)""".toRegex()
 
+data class CraneCommand(
+    val cratesCount: Int,
+    val sourceStack: ArrayDeque<Crate>,
+    val destinationStack: ArrayDeque<Crate>
+)
+
 fun main() {
-    fun part1(input: List<String>): String {
-        // Parse state representation
+    fun parseInput(input: List<String>): Pair<Array<ArrayDeque<Crate>>, List<CraneCommand>> {
+        // Find out stack count
         val stateRepresentation = input.takeWhile { it.isNotEmpty() }
         val indexLine = stateRepresentation.last()
         val stackCount = indexLine[indexLine.length - 2].digitToInt()
         val stacks = Array(stackCount) { ArrayDeque<Crate>() }
+
+        // Parse state representation
         for (level in (stateRepresentation.size - 2) downTo 0) {
             for (stackId in 0 until stackCount) {
                 val crate = stateRepresentation[level][1 + stackId * 4]
@@ -21,57 +29,60 @@ fun main() {
             }
         }
 
-        // Parse rearrangement procedure
+        // Parse crane commands
+        val craneCommands = ArrayList<CraneCommand>()
         for (commandLine in input.drop(stateRepresentation.size + 1)) {
             val (cratesCount, sourceStackIndex, destinationStackIndex) =
                 commandLineRegex.matchEntire(commandLine)?.groupValues?.drop(1)?.map { it.toInt() }
                     ?: throw IllegalArgumentException("Incorrect command line $commandLine")
-            for (i in 0 until cratesCount) {
-                val movedCrate = stacks[sourceStackIndex - 1].removeLast()
-                stacks[destinationStackIndex - 1].addLast(movedCrate)
-            }
+
+            craneCommands.add(
+                CraneCommand(
+                    cratesCount,
+                    stacks[sourceStackIndex - 1],
+                    stacks[destinationStackIndex - 1]
+                )
+            )
         }
 
-        // Find top of the stacks
+        return Pair(stacks, craneCommands)
+    }
+
+    fun listTopCreates(stacks: Array<ArrayDeque<Crate>>): String {
         val stacksTop = StringBuilder()
         for (stack in stacks)
             stacksTop.append(stack.last())
         return stacksTop.toString()
     }
 
-    fun part2(input: List<String>): String {
-        // Parse state representation
-        val stateRepresentation = input.takeWhile { it.isNotEmpty() }
-        val indexLine = stateRepresentation.last()
-        val stackCount = indexLine[indexLine.length - 2].digitToInt()
-        val stacks = Array(stackCount) { ArrayDeque<Crate>() }
-        for (level in (stateRepresentation.size - 2) downTo 0) {
-            for (stackId in 0 until stackCount) {
-                val crate = stateRepresentation[level][1 + stackId * 4]
-                if (crate != ' ')
-                    stacks[stackId].addLast(crate)
-            }
-        }
+    fun part1(input: List<String>): String {
+        val (stacks, craneCommands) = parseInput(input)
 
-        // Parse rearrangement procedure
-        for (commandLine in input.drop(stateRepresentation.size + 1)) {
-            val (cratesCount, sourceStackIndex, destinationStackIndex) =
-                commandLineRegex.matchEntire(commandLine)?.groupValues?.drop(1)?.map { it.toInt() }
-                    ?: throw IllegalArgumentException("Incorrect command line $commandLine")
-
-            val tempStack = ArrayList<Crate>()
-            for (i in 0 until cratesCount) {
-                val movedCrate = stacks[sourceStackIndex - 1].removeLast()
-                tempStack.add(movedCrate)
+        for (command in craneCommands) {
+            for (i in 0 until command.cratesCount) {
+                val movedCrate = command.sourceStack.removeLast()
+                command.destinationStack.addLast(movedCrate)
             }
-            stacks[destinationStackIndex - 1].addAll(tempStack.reversed())
         }
 
         // Find top of the stacks
-        val stacksTop = StringBuilder()
-        for (stack in stacks)
-            stacksTop.append(stack.last())
-        return stacksTop.toString()
+        return listTopCreates(stacks)
+    }
+
+    fun part2(input: List<String>): String {
+        val (stacks, craneCommands) = parseInput(input)
+
+        for (command in craneCommands) {
+            val tempStack = ArrayList<Crate>()
+            for (i in 0 until command.cratesCount) {
+                val movedCrate = command.sourceStack.removeLast()
+                tempStack.add(movedCrate)
+            }
+            command.destinationStack.addAll(tempStack.reversed())
+        }
+
+        // Find top of the stacks
+        return listTopCreates(stacks)
     }
 
     val testInput = readInput("sample_data", 5)
