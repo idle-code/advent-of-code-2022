63a7ab32435e487e803d7b0b0c169915f01a5753
Pawel Zukowski <p.z.idlecode@gmail.com>
2022-12-10 06:42:12 +0100

    Day 10 part 1 done

--- src/day10/Day10.kt
+++ src/day10/Day10.kt
@@ -5,9 +5,81 @@ import readInput
 
 private const val DAY_NUMBER = 10
 
+
+data class Operation(val opcode: String, val arg: Int? = null)
+
+class Simulator {
+    var registerX = 1
+
+    var currentCycle = 0
+
+    val breakpointValues = ArrayList<Pair<Int, Int>>()
+
+    private val breakpoints = HashSet<Int>()
+
+    fun addBreakpoint(cycle: Int) {
+        breakpoints.add(cycle)
+    }
+
+    fun execute(operations: List<Operation>) {
+        for (operation in operations) {
+            if (currentCycle > 218)
+                currentCycle += 0
+            execute(operation)
+        }
+    }
+
+    private fun breakpoint(currentInstructionCycleCost: Int) {
+        for (cycle in currentCycle+1 ..currentCycle + currentInstructionCycleCost) {
+            if (cycle in breakpoints)
+                breakpointValues.add(Pair(cycle, registerX))
+        }
+    }
+
+    fun execute(operation: Operation) {
+        when (operation.opcode) {
+            "noop" -> {
+                breakpoint(1)
+                currentCycle += 1
+            }
+            "addx" -> {
+                breakpoint(2)
+                registerX += operation.arg!!
+                currentCycle += 2
+            }
+            else -> throw IllegalArgumentException("Unknown opcode: ${operation.opcode}")
+        }
+    }
+}
+
 fun main() {
+    fun parseProgram(rawInput: List<String>): List<Operation> {
+        val program = ArrayList<Operation>()
+        for (line in rawInput) {
+            if (line.startsWith("noop"))
+                program.add(Operation(line))
+            else {
+                val operation = line.substringBefore(' ')
+                val arg = line.substringAfter(' ')
+
+                program.add(Operation(operation, arg.toInt()))
+            }
+        }
+        return program
+    }
+
     fun part1(rawInput: List<String>): Int {
-        return 0
+        val program = parseProgram(rawInput)
+        val checkpoints = listOf(20, 60, 100, 140, 180, 220)
+        val simulator = Simulator()
+        for (point in checkpoints)
+            simulator.addBreakpoint(point)
+        simulator.execute(program)
+        for (kv in simulator.breakpointValues) {
+            println("Cycle ${kv.first}*${kv.second} = ${kv.first * kv.second}")
+        }
+        val signalStrength = simulator.breakpointValues.map { it.first * it.second }.sum()
+        return signalStrength
     }
 
     fun part2(rawInput: List<String>): Int {
@@ -21,10 +93,10 @@ fun main() {
 
     val part1SampleResult = part1(sampleInput)
     println(part1SampleResult)
-    check(part1SampleResult == 0)
+    check(part1SampleResult == 13140)
 
-//    val part1MainResult = part1(mainInput)
-//    println(part1MainResult)
+    val part1MainResult = part1(mainInput)
+    println(part1MainResult)
 //    check(part1MainResult == 0)
 //
 //    val part2SampleResult = part2(sampleInput)
