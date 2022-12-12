c338b7c835fb813ca25558ccfe8abb011adb60e6
Pawel Zukowski <p.z.idlecode@gmail.com>
2022-12-12 09:53:25 +0100

    Day 12 part 2 done

--- src/day12/Day12.kt
+++ src/day12/Day12.kt
@@ -12,6 +12,8 @@ typealias Height = Char
 
 data class Position(val x: Int, val y: Int)
 
+private const val NOT_VISITED = -1
+
 fun main() {
     fun parseMap(rawInput: List<String>): Triple<Array<Array<Height>>, Position, Position> {
         val heightMap = Array(rawInput.size) { Array(rawInput[0].length) { 'a' } }
@@ -23,8 +25,7 @@ fun main() {
                 if (heightMap[y][x] == 'S') {
                     start = Position(x, y)
                     heightMap[y][x] = 'a'
-                }
-                else if (heightMap[y][x] == 'E') {
+                } else if (heightMap[y][x] == 'E') {
                     end = Position(x, y)
                     heightMap[y][x] = 'z'
                 }
@@ -33,7 +34,7 @@ fun main() {
         return Triple(heightMap, start, end)
     }
 
-    fun neighboursOf(stepsMap: Array<Array<Int>>, pos: Position): List<Position> {
+    fun allNeighboursOf(stepsMap: Array<Array<Int>>, pos: Position): List<Position> {
         val positions = listOf(
             Position(pos.x - 1, pos.y),
             Position(pos.x + 1, pos.y),
@@ -42,10 +43,8 @@ fun main() {
         )
         val width = stepsMap[0].size
         val height = stepsMap.size
-        return positions.filter { position ->
-            position.x >= 0 && position.y >= 0
-                &&
-            position.x < width && position.y < height
+        return positions.filter { pos ->
+            pos.x >= 0 && pos.y >= 0 && pos.x < width && pos.y < height
         }
     }
 
@@ -64,70 +63,96 @@ fun main() {
         logln("--------------------------------------------------")
     }
 
-    fun findWay(heightMap: Array<Array<Height>>, start: Position, end: Position): Int {
-        val stepsMap = Array(heightMap.size) { Array(heightMap[0].size) { -1 } }
+    fun findWay(
+        heightMap: Array<Array<Height>>,
+        start: Position,
+        pathExists: (Height, Height) -> Boolean
+    ): Array<Array<Int>> {
+        val stepsMap = Array(heightMap.size) { Array(heightMap[0].size) { NOT_VISITED } }
         stepsMap[start.y][start.x] = 0
-        val visitQueue = ArrayDeque<Position>()
-        visitQueue.addAll(neighboursOf(stepsMap, start))
+        val candidates = ArrayDeque<Position>()
+        candidates.addAll(allNeighboursOf(stepsMap, start))
 
-        while (visitQueue.isNotEmpty()) {
-            val current = visitQueue.removeFirst()
-            var currentSteps = stepsMap[current.y][current.x]
+        while (candidates.isNotEmpty()) {
+            val current = candidates.removeFirst()
+
+            val currentSteps = stepsMap[current.y][current.x]
+            if (currentSteps != NOT_VISITED)
+                continue
+
             val currentHeight = heightMap[current.y][current.x]
-            val neighbours = neighboursOf(stepsMap, current)
-            val reachableNeighbours = neighbours
-                .filter {
-                    val neighbourHeight = heightMap[it.y][it.x]
-                    currentHeight <= (neighbourHeight + 1)
-                }
-            val neighbourSteps = reachableNeighbours
-                .filter { stepsMap[it.y][it.x] != -1 }
-                .map { stepsMap[it.y][it.x] }
-            if (neighbourSteps.isNotEmpty()) {
-                val newMinSteps = neighbourSteps.min() + 1
-                stepsMap[current.y][current.x] = newMinSteps// min(newMinSteps, currentSteps)
-                currentSteps = stepsMap[current.y][current.x]
+
+            // Add neighbours for further processing
+            val allNeighbours = allNeighboursOf(stepsMap, current)
+            val neighboursReachableFromCurrent = allNeighbours.filter {
+                val neighbourHeight = heightMap[it.y][it.x]
+                pathExists(currentHeight, neighbourHeight)
+            }.filter {
+                val neighbourSteps = stepsMap[it.y][it.x]
+                neighbourSteps == NOT_VISITED
+            }.filter {
+                it !in candidates
             }
-            val unvisitedReachableNeighbours = reachableNeighbours
-                .filter { (stepsMap[it.y][it.x] == -1 && !(visitQueue.contains(it))) }
-            visitQueue.addAll(unvisitedReachableNeighbours)
-            if (visitQueue.size % 100 == 0)
-                printStepMap(stepsMap)
+            candidates.addAll(neighboursReachableFromCurrent)
+
+            // Update current distance
+            val sourceNeighbours = allNeighbours.filter {
+                val neighbourHeight = heightMap[it.y][it.x]
+                pathExists(neighbourHeight, currentHeight)
+            }.filter {
+                val neighbourSteps = stepsMap[it.y][it.x]
+                neighbourSteps != NOT_VISITED
+            }
+
+            val minNeighbourSteps = sourceNeighbours.minOfOrNull { stepsMap[it.y][it.x] }
+            if (minNeighbourSteps != null)
+                stepsMap[current.y][current.x] = minNeighbourSteps + 1
         }
 
         printStepMap(stepsMap)
 
-        return stepsMap[end.y][end.x]
+        return stepsMap
     }
 
     fun part1(rawInput: List<String>): Int {
         val (heightMap, start, end) = parseMap(rawInput)
-
-        return findWay(heightMap, start, end)
+        val stepMap = findWay(heightMap, start) { from, to -> (from + 1) >= to }
+        return stepMap[end.y][end.x]
     }
 
     fun part2(rawInput: List<String>): Int {
-        return 0
+        val (heightMap, start, end) = parseMap(rawInput)
+
+        val stepMap = findWay(heightMap, end) { from, to -> from <= (to + 1) }
+        var minStepsToA = stepMap[start.y][start.x]
+        for (y in 0..heightMap.lastIndex) {
+            for (x in 0..heightMap[0].lastIndex) {
+                if (heightMap[y][x] != 'a' || stepMap[y][x] == NOT_VISITED)
+                    continue
+                minStepsToA = kotlin.math.min(minStepsToA, stepMap[y][x])
+            }
+        }
+        return minStepsToA
     }
 
     val sampleInput = readInput("sample_data", DAY_NUMBER)
     val mainInput = readInput("main_data", DAY_NUMBER)
 
-    logEnabled = true
+    logEnabled = false
 
-//    val part1SampleResult = part1(sampleInput)
-//    println(part1SampleResult)
-//    check(part1SampleResult == 31)
+    val part1SampleResult = part1(sampleInput)
+    println(part1SampleResult)
+    check(part1SampleResult == 31)
 
     val part1MainResult = part1(mainInput)
     println(part1MainResult)
-//    check(part1MainResult == 0)
+    check(part1MainResult == 370)
 
-//    val part2SampleResult = part2(sampleInput)
-//    println(part2SampleResult)
-//    check(part2SampleResult == 0)
+    val part2SampleResult = part2(sampleInput)
+    println(part2SampleResult)
+    check(part2SampleResult == 29)
 
-//    val part2MainResult = part2(mainInput)
-//    println(part2MainResult)
-//    check(part2MainResult == 0)
+    val part2MainResult = part2(mainInput)
+    println(part2MainResult)
+    check(part2MainResult == 363)
 }
